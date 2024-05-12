790b2fe4ee9b735ddedc30ae24b29f06b710594f
brettp <brettperry7@gmail.com>
2024-05-12 10:21:26 -0500

    first commit - added frontend and backend dirs

---
+++ backend/main.py
@@ -0,0 +1,21 @@
+from typing import List
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Person(BaseModel):
+    id: int
+    name: str
+    age: int
+
+DB: List[Person] = [
+    Person(id=1, name="Chewie", age=15),
+    Person(id=2, name="Tootsie", age=2)
+]
+
+@app.get("/api")
+def read_root():
+    return DB
+
